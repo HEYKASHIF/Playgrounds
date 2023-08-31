48857e0b288160e46f9917f507df881e3a82fb4f
HEYKASHIF <kashifmohd269@gmail.com>
2023-08-31 16:42:59 +0530

    first commit3

---
+++ istDay.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var coleridge = """
+Water, water, everywhere
+and not a drop to drink
+
+"""
+
+let TCel = 30
+print("Temperature in celcius \(TCel)°")
+let TFer = (TCel*9)/5
+print("Temperature in Farenhiet \(TFer + 32)°")
