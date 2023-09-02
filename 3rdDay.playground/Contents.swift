c3678ecf5412bea92923fd07958421d1b2ec1b08
HEYKASHIF <kashifmohd269@gmail.com>
2023-09-02 12:52:37 +0530

    first commit6

---
+++ 3rdDay.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var array: Array = ["akhil", "kashif", "bhaskar", "himani", "ashish", "pallav", "suraj", "akhil"]
+
+print(array.count)
+
+print(Set(array))
