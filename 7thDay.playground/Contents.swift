edc9577f056761ca6c4187088a4d6f0974d8bd01
HEYKASHIF <kashifmohd269@gmail.com>
2023-09-08 17:15:35 +0530

    first commit4

--- 7thDay.playground/Contents.swift
+++ 7thDay.playground/Contents.swift
@@ -1,6 +1,12 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+enum throwError: Error
+{
+    case outOfBounds
+    
+}
+
 func sqrtt(number: Double) throws -> Double
 {
     return sqrt(number)
