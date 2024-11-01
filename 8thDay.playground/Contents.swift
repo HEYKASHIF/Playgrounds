f5c73230606d90ed3817b494b172dd76aaa3e241
HEYKASHIF <kashifmohd269@gmail.com>
2024-11-01 14:01:16 +0530

    restarting playground practice

---
+++ 8thDay.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+/*Write a Swift program to compute the sum of the two integers. If the values are equal return the triple their sum.*/
+
+let  number1 = 10
+let  number2 = 20
+
+let sumOfTwoNumbers = number1 + number2
+
+print("Sum of Two numbers is: \(sumOfTwoNumbers)")
